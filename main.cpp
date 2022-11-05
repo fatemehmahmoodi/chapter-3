d101f278bb8698cbce8b02a61d8583ed80160caa
fatemehmahmoodi <116880900+fatemehmahmoodi@users.noreply.github.com>
2022-11-05 18:28:58 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+     float num1;
+    cout<<" Enter your Number :";
+    cin>>num1;
+    int num2=num1;
+    float num3=num1-num2;
+    cout<< "sahih : "<<num2<<"      "<<"ashari : "<<num3<<endl;
+
+
+    getch();
+    return 0;
+}
